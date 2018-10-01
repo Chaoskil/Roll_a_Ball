9452b3f00ed51ed602fa683372064fccc1187cce
Jonathan Way <jonathan.way@loop.colum.edu>
2018-10-01 17:46:46 -0500

    Player Movement
    - Created player controller script
    - adjusted the code to allow for precise speed

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    [SerializeField]
+    private float Speed;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Verticle");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
+
+        rb.AddForce(movement * Speed);
+    }
+}
