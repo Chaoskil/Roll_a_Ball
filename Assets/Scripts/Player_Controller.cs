6a37de9ba02a198efa37c48f0ccdccd5077c85a1
Jonathan Way <jonathan.way@loop.colum.edu>
2018-10-01 19:05:05 -0500

    collecting and counting
    - edited the player script so the player can pick up collectibles and so  the game can track how many
    - created a canvas with a text displaying current collected collectibles
    - tagged collectibles as "Pick Ups" so they can be collected
    -adjusted rigibodies and colliders of collectibles and player

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,27 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Controller : MonoBehaviour {
 
     [SerializeField]
     private float Speed;
 
+    [SerializeField]
+    private Text countText;
+
     private Rigidbody rb;
+    private int Count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Count = 0;
+        SetCountText();
     }
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVerticle = Input.GetAxis("Vertical");
 
-
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
 
         rb.AddForce(movement * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            Count = Count + 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + Count.ToString();
     }
 }
